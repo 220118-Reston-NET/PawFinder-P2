7efd23625ab4bbdea31407e126f877d0cd5713bb
Andydemarks <andydemarks@andydemarks.com>
2022-03-09 16:09:55 -0500

    added login/sign up screen

---
+++ FrontEnd/home/src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginSignUpHeading:String = "Login:"
+  newOrExistingUser:String = "Don't have an account?"
+  loginOrRegister:String = "Create an Account"
+
+  show:boolean = false;
+
+  ngOnInit(): void {
+  }
+
+  showPassword()
+  {
+    this.show = !this.show;
+  } 
+
+  switchBetweenLoginAndSignUp()
+  {
+    if(this.loginSignUpHeading === "Login:")
+    {
+      this.loginSignUpHeading = "Sign Up:";
+      this.newOrExistingUser = "Already have an account?";
+      this.loginOrRegister = "Back to Login Page";
+    }
+    else
+    {
+      this.loginSignUpHeading = "Login:";
+      this.newOrExistingUser = "Don't have an account?";
+      this.loginOrRegister = "Create an Account";
+    }
+  }
+
+}
