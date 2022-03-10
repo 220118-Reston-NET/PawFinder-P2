d6cf7193b55f8ff4a6d79d06b33106cd1cb44d29
Nicolas Colorado <nicolasaug.colorado@gmail.com>
2022-03-10 17:57:03 -0500

    Included XML documentation in BL/DL

--- Backend/PawFinderBL/IUserBL.cs
+++ Backend/PawFinderBL/IUserBL.cs
@@ -4,30 +4,80 @@ namespace PawFinderBL;
 
 public interface IUserBL
 {
-  
     List<User> GetAllUsers();
     User GetUser(int UserID);
     User RegisterUser(User p_user);
     List<User> SearchUser(string p_name);
     List<User> ViewMatchedUser(int userID);
     List<Message> GetConversation(int UserID1, int UserID2);
-    
-    // string GenerateFileName(string fileName, string userName);
     User UpdateUser(User user);
     Message AddMessage(Message message);
     List<User> GetPotentialMatch(User p_user);
 
+
     //Async version functions
+
+    /// <summary>
+    /// Will give back all users in the form of a list.
+    /// </summary>
+    /// <returns> Returns all users in the database. </returns>
     Task<List<User>> GetAllUsersAsync();
+
+    /// <summary>
+    /// Will give back a user from userID.
+    /// </summary>
+    /// <param name="UserID"></param>
+    /// <returns> Returns an individual user. </returns>
     Task<User> GetUserAsync(int UserID);
+
+    /// <summary>
+    /// Adds a user to the database.
+    /// </summary>
+    /// <param name="p_user"></param>
+    /// <returns> Returns a user that was added. </returns>
     Task<User> RegisterUserAsync(User p_user);
+
+    /// <summary>
+    /// Will search for a user from a username.
+    /// </summary>
+    /// <param name="p_name"></param>
+    /// <returns> Returns a user that was searched. </returns>
     Task<List<User>> SearchUserAsync(string p_name);
+
+    /// <summary>
+    /// Will give back matched user from userID.
+    /// </summary>
+    /// <param name="userID"></param>
+    /// <returns> Returns a matched user. </returns>
     Task<List<User>> ViewMatchedUserAsync(int userID);
+
+    /// <summary>
+    /// Will give back a conversation between two users.
+    /// </summary>
+    /// <param name="UserID1"></param>
+    /// <param name="UserID2"></param>
+    /// <returns> Returns an a conversation. </returns>
     Task<List<Message>> GetConversationAsync(int UserID1, int UserID2);
+
+    /// <summary>
+    /// Updates a user to the database.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns> Returns updated user information. </returns>
     Task<User> UpdateUserAsync(User user);
+
+    /// <summary>
+    /// Adds a new message between sender and receiver.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns> Returns a new message that was added. </returns>
     Task<Message> AddMessageAsync(Message message);
+
+    /// <summary>
+    /// Will give back a potential match based on user.
+    /// </summary>
+    /// <param name="p_user"></param>
+    /// <returns> Returns a potential match. </returns>
     Task<List<User>> GetPotentialMatchAsync(User p_user);
 
-
-    
 }
--- Backend/PawFinderDL/IRepository.cs
+++ Backend/PawFinderDL/IRepository.cs
@@ -13,14 +13,56 @@ public interface IRepository
     Message AddMessage(Message message);
 
 
-
     //Async version functions
+
+
+    /// <summary>
+    /// Adds a user to the database.
+    /// </summary>
+    /// <param name="p_user"></param>
+    /// <returns> Returns a user that was added. </returns>
     Task<User> RegisterUserAsync(User p_user);
+
+    /// <summary>
+    /// Will give back all users in the form of a list.
+    /// </summary>
+    /// <returns> Returns all users in the database. </returns>
     Task<List<User>> GetAllUsersAsync();
+
+    /// <summary>
+    /// Will give back a user from userID.
+    /// </summary>
+    /// <param name="UserID"></param>
+    /// <returns> Returns an individual user. </returns>
     Task<User> GetUserAsync(int UserID);
+
+    /// <summary>
+    /// Will give back matched user from userID.
+    /// </summary>
+    /// <param name="userID"></param>
+    /// <returns> Returns a matched user. </returns>
     Task<List<User>> ViewMatchedUserAsync(int UserID);
+
+    /// <summary>
+    /// Will give back a conversation between two users.
+    /// </summary>
+    /// <param name="UserID1"></param>
+    /// <param name="UserID2"></param>
+    /// <returns> Returns an a conversation. </returns>
     Task<List<Message>> GetConversationAsync(int UserID1, int UserID2);
+
+    /// <summary>
+    /// Updates a user to the database.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns> Returns updated user information. </returns>
     Task<User> UpdateUserAsync(User p_user);
+
+    /// <summary>
+    /// Adds a new message between sender and receiver.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns> Returns a new message that was added. </returns>
     Task<Message> AddMessageAsync(Message message);
 
 }
