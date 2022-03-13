ae0b5afe264e16bfec6f8b0ec600eb4e0b944f2e
Hy0tic <vothanhdat278@gmail.com>
2022-03-13 18:29:06 -0400

    AddLikedUser functionality

--- Backend/PawFinderAPI/Controllers/PawFinderController.cs
+++ Backend/PawFinderAPI/Controllers/PawFinderController.cs
@@ -162,6 +162,22 @@ namespace PawFinderAPI.Controllers
             }
         }
 
+        // POST: api/PawFinder
+        [HttpPost("AddLikedUser")]
+        public async Task<IActionResult> AddLikedUserAsync(int LikerID, int LikedID)
+        {
+            try
+            {
+                Log.Information("Successfully added liked user");
+                return Ok(await _userBL.AddLikedUserAsync(LikerID, LikedID));
+            }
+            catch (System.Exception ex)
+            {
+                Log.Warning("Error adding Liked User");
+                return Conflict(ex.Message);
+            }
+        }
+
         // POST: api/PawFinder/2
         [HttpPost("AddMessage")]
         public async Task<IActionResult> AddMessageAsync([FromBody] Message message)
@@ -239,7 +255,7 @@ namespace PawFinderAPI.Controllers
         }
 
 
-        [HttpPut("LogIn")]
+        [HttpGet("LogIn")]
         public async Task<IActionResult> LoginAsync(string UserNameInput, string PasswordInput)
         {
             List<User> ListOfAllUsers = await _userBL.GetAllUsersAsync();
