43fb409845741c4a338d39e1f74228f7e966d3c3
Andydemarks <andydemarks@andydemarks.com>
2022-03-14 11:49:29 -0400

    added functionality to like and dislike buttons for api

--- FrontEnd/home/src/app/like/like.component.ts
+++ FrontEnd/home/src/app/like/like.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Users } from '../models/users.model';
+import { LikeService } from '../services/like.service';
 import { NavbarService } from '../services/navbar.service';
 import { UserService } from '../services/user.service';
 
@@ -9,12 +10,15 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
- @Input()Like:number | undefined;
+  @Input()Like:number | undefined;
+  
+  myUserID:number = 2;
+  
+  listOfUsers:Users[];
+  sizeOfUsersList:number = 0;
+ 
 
- listOfUsers:Users[];
- sizeOfUsersList:number = 0;
-
-  constructor(public nav: NavbarService, private userService: UserService) { this.listOfUsers = []; }
+  constructor(public nav: NavbarService, private likeService: LikeService, private userService: UserService) { this.listOfUsers = []; }
 
   ngOnInit(): void {
     
@@ -26,6 +30,14 @@ export class LikeComponent implements OnInit {
 
   }
 
-  
+  likeUser(p_likeeID:number, p_likerID:number)
+  {
+    this.likeService.likeAUser(p_likeeID, p_likerID).subscribe();
+  }
+
+  dislikeUser(p_passeeID:number, p_passerID:number)
+  {
+    this.likeService.DislikeAUser(p_passeeID, p_passerID).subscribe();
+  }
 
 }
