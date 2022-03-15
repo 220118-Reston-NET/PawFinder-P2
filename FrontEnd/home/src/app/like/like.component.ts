c1bc8d18cbf2a2fb1b675420f0065b581e8f5be8
Andydemarks <andydemarks@andydemarks.com>
2022-03-15 17:45:03 -0400

    updated the like page to filter using backend filtering functionality as well as if button is pressed update the page.

--- FrontEnd/home/src/app/like/like.component.ts
+++ FrontEnd/home/src/app/like/like.component.ts
@@ -19,38 +19,31 @@ export class LikeComponent implements OnInit {
   myUserID:number = GlobalComponent.loggedInUserID;
   
   listOfUsers:Users[];
-  listOfMatchedUsers:Users[];
-  filteredListOfUsers:Users[];
   sizeOfUsersList:number = 0;
 
   constructor(public nav: NavbarService, private likeService: LikeService, private userService: UserService, private matchservice: MatchService) { 
-    this.listOfUsers = []; this.listOfMatchedUsers= []; this.filteredListOfUsers = []; }
+    this.listOfUsers = []; }
 
   ngOnInit(): void {
     
     this.nav.show();
 
-    this.userService.getAllUsers().subscribe(result => {console.log(result); this.listOfUsers = result});
-
-    this.matchservice.getMatches(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfMatchedUsers = result;});
-
-    this.filteredListOfUsers = this.listOfUsers.filter( x => !this.listOfMatchedUsers.includes(x) );
+    this.userService.getPotentialMatch(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfUsers = result});
 
     this.sizeOfUsersList = this.listOfUsers.length;
-
-
 
   }
 
   likeUser(p_likeeID:number, p_likerID:number)
   {
     this.likeService.likeAUser(p_likeeID, p_likerID).subscribe();
+    this.userService.getPotentialMatch(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfUsers = result});
   }
 
   dislikeUser(p_passeeID:number, p_passerID:number)
   {
     this.likeService.DislikeAUser(p_passeeID, p_passerID).subscribe();
-
+    this.userService.getPotentialMatch(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfUsers = result});
   }
 
 }
