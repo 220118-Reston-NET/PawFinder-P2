d2e16756772aeb53ffad5113ab4172031368de6f
Andrew DeMarco <97972359+andydemarks@users.noreply.github.com>
2022-04-11 18:01:01 -0400

    Updated like page to show one user at a time
    Updated the like page to only show one user at a time rather than the entire list of likeable users at once. Makes for a easier to use interface and cleaner page.

--- FrontEnd/home/src/app/like/like.component.ts
+++ FrontEnd/home/src/app/like/like.component.ts
@@ -21,6 +21,16 @@ export class LikeComponent implements OnInit {
   listOfUsers:Users[];
   sizeOfUsersList:number = 0;
 
+  userID:number = 0;
+  userName:string = "";
+  userDOB:Date = new Date;
+  userBio:string = "";
+  userSize:string = "";
+  userBreed:string = "";
+  userImg:string = "";
+
+  i:number = 0;
+
   constructor(public nav: NavbarService, private likeService: LikeService, private userService: UserService, private matchservice: MatchService) { 
     this.listOfUsers = []; }
 
@@ -28,22 +38,74 @@ export class LikeComponent implements OnInit {
     
     this.nav.show();
 
-    this.userService.getPotentialMatch(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfUsers = result});
+    this.userService.getPotentialMatch(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfUsers = result;
+      this.userID = result[0].userID;
+      this.userName = result[0].userName;
+      this.userDOB = result[0].userDOB;
+      this.userBio= result[0].userBio;
+      this.userSize = result[0].userSize;
+      this.userBreed = result[0].userBreed;
+      this.userImg = result[0].userImg;
+    });
 
     this.sizeOfUsersList = this.listOfUsers.length;
-
+  
   }
 
   likeUser(p_likeeID:number, p_likerID:number)
   {
     this.likeService.likeAUser(p_likeeID, p_likerID).subscribe();
-    this.userService.getPotentialMatch(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfUsers = result});
+    
+    this.i = this.i + 1;
+
+    let currentUser:Users=
+    {
+      userID: this.listOfUsers[this.i].userID,
+      userName:this.listOfUsers[this.i].userName,
+      userPassword:"",
+      userDOB: this.listOfUsers[this.i].userDOB,
+      userBio:this.listOfUsers[this.i].userBio,
+      userBreed:this.listOfUsers[this.i].userBreed,
+      userSize:this.listOfUsers[this.i].userSize,
+      userImg:""
+    }
+
+    this.userID = currentUser.userID;
+    this.userName = currentUser.userName;
+    this.userDOB = currentUser.userDOB;
+    this.userBio= currentUser.userBio;
+    this.userSize = currentUser.userSize;
+    this.userBreed = currentUser.userBreed;
+    this.userImg = currentUser.userImg;
+    
   }
 
   dislikeUser(p_passeeID:number, p_passerID:number)
   {
     this.likeService.DislikeAUser(p_passeeID, p_passerID).subscribe();
-    this.userService.getPotentialMatch(GlobalComponent.loggedInUserID).subscribe(result => {console.log(result); this.listOfUsers = result});
+
+    this.i = this.i + 1;
+
+    let currentUser:Users=
+    {
+      userID: this.listOfUsers[this.i].userID,
+      userName:this.listOfUsers[this.i].userName,
+      userPassword:"",
+      userDOB: this.listOfUsers[this.i].userDOB,
+      userBio:this.listOfUsers[this.i].userBio,
+      userBreed:this.listOfUsers[this.i].userBreed,
+      userSize:this.listOfUsers[this.i].userSize,
+      userImg:""
+    }
+
+    this.userID = currentUser.userID;
+    this.userName = currentUser.userName;
+    this.userDOB = currentUser.userDOB;
+    this.userBio= currentUser.userBio;
+    this.userSize = currentUser.userSize;
+    this.userBreed = currentUser.userBreed;
+    this.userImg = currentUser.userImg;
+
   }
 
 }
